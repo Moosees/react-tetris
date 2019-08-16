d82c12a1a4d37ffe18df11a549459557ac6acb48
Moosees <linus.almgren@gmail.com>
2019-08-16 18:51:27 +0200

    Add memoization to cells

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { TETROMINOS } from '../tetrominos';
 import { StyledCell } from './cell.styles';
 
-const Cell = ({ type }) => (
-  <StyledCell color={TETROMINOS[type].color} />
-);
+const Cell = ({ type }) => <StyledCell color={TETROMINOS[type].color} />;
 
-export default Cell;
+export default React.memo(Cell);
