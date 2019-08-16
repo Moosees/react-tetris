daa01a73c97d0913ee2a223185a7d47e3fdc4fb4
Moosees <linus.almgren@gmail.com>
2019-08-16 16:54:52 +0200

    Add effect to sweep filled rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,28 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = stageToSweep =>
+      stageToSweep.reduce((accumulator, row) => {
+        // returns -1 if not found, else found index
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          // add empty row instead of the full row
+          accumulator.unshift(
+            new Array(stageToSweep[0].length).fill([0, 'clear'])
+          );
+          return accumulator;
+        } else {
+          // if row is not full just add it to newStage
+          accumulator.push(row);
+          return accumulator;
+        }
+      }, []);
+
     const updateStage = prevStage => {
       const newStage = prevStage.map(row =>
         row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
@@ -21,8 +41,10 @@ export const useStage = (player, resetPlayer) => {
       });
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
+      } else {
+        return newStage;
       }
-      return newStage;
     };
     setStage(prev => updateStage(prev));
   }, [player, resetPlayer]);
