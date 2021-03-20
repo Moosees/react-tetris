e73d87db4579fc27440a68af0165def725dbe597
Moosees <linus.almgren@gmail.com>
2021-03-20 06:07:45 +0100

    npm audit fix, fix callback warning

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,18 +1,17 @@
 import { useState, useEffect, useCallback } from 'react';
 
-export const useGameStatus = rowsCleared => {
+export const useGameStatus = (rowsCleared) => {
   const [score, setScore] = useState(0);
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const LINE_POINTS = [40, 100, 300, 1200];
-
   const calcScore = useCallback(() => {
+    const LINE_POINTS = [40, 100, 300, 1200];
     if (rowsCleared) {
-      setScore(prev => prev + LINE_POINTS[rowsCleared - 1] * (level + 1));
-      setRows(prev => prev + rowsCleared);
+      setScore((prev) => prev + LINE_POINTS[rowsCleared - 1] * (level + 1));
+      setRows((prev) => prev + rowsCleared);
     }
-  }, [level, LINE_POINTS, rowsCleared]);
+  }, [level, rowsCleared]);
 
   useEffect(() => {
     calcScore();
